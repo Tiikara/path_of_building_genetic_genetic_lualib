8f5d576d399bcd92bb52c9181a97470daf33abef
Evgeniy Simonenko <tiikara93@gmail.com>
2023-02-07 13:04:25 +1000

    fix sorting ens_nondominating_sorting

--- src/mo/ens_nondominating_sorting.rs
+++ src/mo/ens_nondominating_sorting.rs
@@ -1,18 +1,32 @@
 use std::cmp::Ordering;
 use crate::mo::Solution;
 
-fn dominates(vals1: &Vec<f64>, vals2: &Vec<f64>) -> bool {
-    vals1.iter().zip(vals2).all(|(v1, v2)| *v1 <= *v2) &&
-        vals1.iter().zip(vals2).any(|(v1, v2)| *v1 < *v2)
+fn dominates(vals1: &Vec<f64>, other: &Vec<f64>) -> bool {
+    let mut self_better = false;
+    let mut equal = false;
+    for (i, obj) in vals1.iter().enumerate() {
+        match obj.partial_cmp(&other[i]).unwrap() {
+            Ordering::Less => self_better = true,
+            Ordering::Equal => equal = true,
+            Ordering::Greater => return false,
+        }
+    }
+    self_better && !equal
 }
 
-pub fn ens_nondominated_sorting(pop: &mut Vec<Vec<f64>>) -> Vec<Vec<usize>> {
-    pop.sort_unstable_by(|a, b| a.first().unwrap().partial_cmp(&b.first().unwrap()).unwrap());
+pub fn ens_nondominated_sorting(pop: &Vec<Vec<f64>>) -> Vec<Vec<usize>> {
+    let mut pop_i: Vec<(usize, &Vec<f64>)> = pop.iter().enumerate()
+        .map(|(i, p)| (i, p))
+        .collect();
+
+    pop_i.sort_unstable_by(|a, b| a.1.first().unwrap().partial_cmp(&b.1.first().unwrap()).unwrap());
+
+    let pop: Vec<&Vec<f64>> = pop_i.iter().map(|p| p.1).collect();
 
     let mut fronts = vec![];
 
     for (n, p) in pop.iter().enumerate() {
-        let k = sequential_search(pop, p, &fronts);
+        let k = sequential_search(&pop, p, &fronts);
         if k == fronts.len() {
             fronts.push(vec![n]);
         } else {
@@ -20,10 +34,12 @@ pub fn ens_nondominated_sorting(pop: &mut Vec<Vec<f64>>) -> Vec<Vec<usize>> {
         }
     }
 
-    fronts
+    fronts.iter()
+        .map(|front| front.iter().map(|i| pop_i[*i].0 ).collect())
+        .collect()
 }
 
-fn sequential_search(pop: &Vec<Vec<f64>>,p: &Vec<f64>, fronts: &[Vec<usize>]) -> usize {
+fn sequential_search(pop: &Vec<&Vec<f64>>, p: &Vec<f64>, fronts: &[Vec<usize>]) -> usize {
     let mut k = 0;
     let x = fronts.len();
     while k < x {
